234efb52177473de7c34fd33b3761981460c2c80
Ricardo Gavira <ricgavira@hotmail.com>
2023-08-11 21:08:43 -0400

    Criando estrutura inicial

--- Thiesen.Domain/Entities/BaseEntity.cs
+++ Thiesen.Domain/Entities/BaseEntity.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Thiesen.Domain.Entities
+﻿namespace Thiesen.Domain.Entities
 {
     public abstract class BaseEntity
     {
         protected BaseEntity() { }
         public int Id { get; private set; }
+        public DateTime CreateAt { get; private set; }
     }
 }
